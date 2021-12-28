7ff289786206a6d747db97f6550d9e1b04cd9cae
egnam <egnam@pythagraph.com>
2021-12-28 18:09:09 +0900

    가짜 posts 오브젝트에서 axios를 사용한 fake json post 호출로 변경

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,35 +1,34 @@
-// n 밀리세켄드동안 기다리는 프로미스를 만들어주는 함수
-const sleep = (n) => new Promise((resolve) => setTimeout(resolve, n));
+import axios from "axios";
 
-// 가짜 데이터
-const posts = [
-  {
-    id: 1,
-    title: "리덕스 미들웨어를 배워봅시다",
-    body: "리덕스 미들웨어를 직접 만들어보면 이해하기 쉽죠.",
-  },
-  {
-    id: 2,
-    title: "redux-thunk를 사용해봅시다",
-    body: "redux-thunk를 사용해서 비동기 작업을 처리해봅시다.",
-  },
-  {
-    id: 3,
-    title: "redux-saga도 사용해봅시다.",
-    body: "나중엔 redux-saga를 사용해서 비동기 작업을 처리하는 방법도 배워볼 거예요.",
-  },
-];
+// n 밀리세켄드동안 기다리는 프로미스를 만들어주는 함수
+// const sleep = (n) => new Promise((resolve) => setTimeout(resolve, n));
 
 // 비동기함수
 export const getPosts = async () => {
-  await sleep(500);
-  // 0.5초 쉬고 반환
-  return posts;
+  try {
+    const data = await axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(function (response) {
+        return response.data;
+      });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 // id로 조회하는 비동기 함수
 export const getPostById = async (id) => {
-  await sleep(500);
-  // 0.5초 쉬고 반환
-  return posts.find((post) => post.id === id); // id로 찾아서 반환
+  try {
+    const data = await axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then(function (response) {
+        return response.data;
+      });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
