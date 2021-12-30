7161cbe312aadd3f1497465111efc4cd6f002e7e
egnam <egnam@pythagraph.com>
2021-12-30 10:15:14 +0900

    posts thunk함수 saga 함수로 수정

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,12 +2,12 @@ import { combineReducers } from "redux";
 import { all } from "redux-saga/effects";
 
 import counter, { counterSaga } from "./counter";
-import posts from "./posts";
+import posts, { postsSaga } from "./posts";
 
 const rootReducer = combineReducers({ counter, posts });
 
 export function* rootSaga() {
-  yield all([counterSaga()]); // all은 배열 안의 여러 사가를 동시에 실행
+  yield all([counterSaga(), postsSaga()]); // all은 배열 안의 여러 사가를 동시에 실행
 }
 
 export default rootReducer;
--- src/modules/posts.js
+++ src/modules/posts.js
@@ -6,11 +6,7 @@ import {
   handleAsyncActionsById,
   reducerUtils,
 } from "../lib/asyncUtils";
-
-// 프로미스를 다루는 리덕스 모듈을 다룰때 고려해야하는 사항
-// 1. 프로미스가 시작, 성공, 실패했을때 다른 액션을 디스패치
-// 2. 각 프로미스마다 thunk 함수를 만들어줘야한다
-// 3. 리듀서에서 액션에 따라 로딩중, 결과, 에러 상태를 변경해주어야 합니다
+import { call, put, takeEvery } from "redux-saga/effects";
 
 // 포스트 여러개 조회하기
 const GET_POSTS = "GET_POSTS"; // 요청 시작
@@ -25,17 +21,66 @@ const GET_POST_ERROR = "GET_POST_ERROR";
 // 포스트 조회시 재로딩 문제를 해결하기 위한 포스트 비우기 액션
 const CLEAR_POST = "CLEAR_POST";
 
-// thunk를 사용할때 꼭 모든 액션들에 대하여 액션 생섬함수를 만들필요 없이 바로 thunk 함수 안에서 만들어도됨
-// Thunk 함수들 리팩토링
-export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = createPromiseThunkById(GET_POST, postsAPI.getPostById);
+// redux saga 사용하기
+// 액션 생성함수
+export const getPosts = () => ({ type: GET_POSTS });
+export const getPost = (id) => ({ type: GET_POST, payload: id, meta: id }); // payload는 파라미터 용도(꼭 payload이라고 명칭하지 않아도됨), meta는 리듀서에서 id를 얻기위한 용도
+
+// 제너레이터 함수 (리덕스 사가)
+function* getPostsSaga() {
+  try {
+    const posts = yield call(postsAPI.getPosts); // call을 사용하면 특정 함수를 호출하고, 결과물이 반환 될 때가지 기다려줄 수 있다
+
+    yield put({
+      type: GET_POSTS_SUCCESS,
+      payload: posts,
+    }); // 성공 액션 디스패치
+  } catch (e) {
+    yield put({
+      type: GET_POSTS_ERROR,
+      error: true,
+      payload: e,
+    }); // 실패 액션 디스패치
+  }
+}
+
+// 액션이 지니고 있는 값을 조회하고 싶다면 action을 파라미터로 받아와서 사용 가능
+// 액션에서 payload 값을 추출하여 API를 호출할때 인자로 넣어서 호출함
+// meta 값은 handleAsyncActionsByI를 호환시키기 위함이다
+function* getPostSaga(action) {
+  const param = action.payload;
+  const id = action.meta;
+
+  try {
+    // API 함수에 넣어주고 싶은 인자는 call 함수의 두번째 인자부터 순서대로 넣어주면 된다
+    const post = yield call(postsAPI.getPostById, param);
+    yield put({
+      type: GET_POST_SUCCESS,
+      payload: post,
+      meta: id,
+    });
+  } catch (e) {
+    yield put({
+      type: GET_POST_ERROR,
+      error: true,
+      payload: e,
+      meta: id,
+    });
+  }
+}
+
+// 사가들을 합치기
+export function* postsSaga() {
+  yield takeEvery(GET_POSTS, getPostsSaga);
+  yield takeEvery(GET_POST, getPostSaga);
+}
 
 // 포스트 비우기 액션 생성 함수
 export const clearPost = () => ({ type: CLEAR_POST });
 
 const initialState = {
   posts: reducerUtils.initial(),
-  post: {},
+  post: reducerUtils.initial(),
 };
 
 // 리듀서
@@ -44,9 +89,6 @@ export default function posts(state = initialState, action) {
     case GET_POSTS:
     case GET_POSTS_SUCCESS:
     case GET_POSTS_ERROR:
-      // const postsReducer = handleAsyncActions(GET_POSTS, 'posts');
-      // return postsReducer(state, action);
-      // 위 주석을 간단히 하면
       return handleAsyncActions(GET_POSTS, "posts", true)(state, action);
     case GET_POST:
     case GET_POST_SUCCESS:
